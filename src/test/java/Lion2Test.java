c9f0ca5061cff7f1cb716f940f5889a3517d06f7
BaranovNS <BaranovNS@msk.bcs.ru>
2023-04-27 05:27:55 +0300

    6 Спринт исправления

---
+++ src/test/java/Lion2Test.java
@@ -0,0 +1,34 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class Lion2Test {
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getKittensTest(){
+        Lion lion = new Lion(feline);
+        Mockito.when(feline.getKittens()).thenReturn(3);
+        int expected = 3;
+        Assert.assertEquals(expected, lion.getKittens());
+    }
+
+    @Test
+    public void getFoodLionTest() throws Exception {
+        Lion lion = new Lion(feline);
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        List<String>expected = List.of("Животные", "Птицы", "Рыба");
+        Assert.assertEquals(expected, lion.getFood());
+    }
+
+
+}
---
+++ src/test/java/LionTest.java
@@ -0,0 +1,39 @@
+import com.example.Lion;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+
+@RunWith(Parameterized.class)
+public class LionTest {
+    String sexLion;
+    boolean expectedResult;
+
+    public LionTest(String sexLion, boolean expectedResult){
+        this.sexLion = sexLion;
+        this.expectedResult = expectedResult;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][]getSex(){
+        return new Object[][]{
+                {"Самец", true},
+                {"Самка", false}
+
+        };
+    }
+
+    @Test
+    public void doesHaveManeTest() throws Exception {
+        Lion lion = new Lion(sexLion);
+        Assert.assertEquals(expectedResult, lion.doesHaveMane());
+
+
+    }
+
+
+
+
+
+}
